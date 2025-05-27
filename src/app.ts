b62569a373a23d073afeac5ce78e0d275428e81f
matheusouzag <mathsouzag@gmail.com>
2025-05-27 14:02:33 -0300

    Cors e ajustes de código

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import routes from './routes';
+import cors from 'cors';
 import { AppDataSource } from './database/data-source';
 import 'reflect-metadata';
 
@@ -8,7 +9,15 @@ AppDataSource.initialize().then(() => {
 });
 
 const app = express();
+
+app.use(cors());
+
 app.use(express.json());
 app.use(routes);
+
+app.post("/accounts", (req, res) => {
+  console.log(req.body);
+  res.status(201).json({ message: "Conta criada com sucesso!" });
+});
 
 export default app;
